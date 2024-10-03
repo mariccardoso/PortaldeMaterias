2b3a565954490e189f287c77fe81187fd543aba0
Mariana Cardoso <marianacardoso.tds2.senai@gmail.com>
2024-10-03 10:52:51 -0300

    feat(dica.js): add card2 js

--- assets/js/dica.js
+++ assets/js/dica.js
@@ -1,9 +1,19 @@
 // Seleciona o botão e o conteúdo da notícia
-const noticia1 = document.getElementById('dica1');
+const dica1 = document.getElementById('dica1');
 const icone1 = document.getElementById('dica1');
 
 // Adiciona o evento de clique no ícone
 icone1.addEventListener('click', function() {
     // Alterna a classe 'ativa' para expandir e mudar a cor
     dica1.classList.toggle('ativa');
+});
+
+// Seleciona o botão e o conteúdo da notícia
+const dica2 = document.getElementById('dica2');
+const icone2 = document.getElementById('dica2');
+
+// Adiciona o evento de clique no ícone
+icone2.addEventListener('click', function() {
+    // Alterna a classe 'ativa' para expandir e mudar a cor
+    dica2.classList.toggle('ativa');
 });
